15a3c66f944ee8439981440a7be25773827fe076
yjleeinkr <lucy.lee930216@gmail.com>
2022-10-09 18:00:21 +0900

    make useRouter to change url and context

---
+++ react-router/src/hooks/useRouter.ts
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import RouterContext from "../router/RouterContext";
+
+const useRouter = () => {
+  const { location, setLocation } = useContext(RouterContext);
+
+  const push = (url: string) => {
+    if (location === url) return;
+    history.pushState(url, "", url);
+    setLocation(url);
+  };
+
+  return { push };
+};
+
+export default useRouter;
