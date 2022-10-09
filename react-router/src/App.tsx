3a2a6a8ef17bc77b2c57934fccde6633b8ec5280
yjleeinkr <lucy.lee930216@gmail.com>
2022-10-09 18:05:27 +0900

    used Router, Route referring to react-router-dom

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import HistoryAPI from "./components/historyAPI";
+import HistoryAPI from "./components/HistoryAPI";
+import Router from "./router/Router";
+import Route from "./router/Route";
+import Main from "./components/Main";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+      </Router>
       <HistoryAPI />
     </div>
   );
